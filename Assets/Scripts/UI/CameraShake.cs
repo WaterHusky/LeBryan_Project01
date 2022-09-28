7369212aebaf261cd9695c9456e338ef97abe629
Bryan Le <bryan.le7826@gmail.com>
2022-09-27 21:48:37 -0500

    Added in camera shake and healthbars

--- Assets/Scripts/UI/CameraShake.cs
+++ Assets/Scripts/UI/CameraShake.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Health playerHealth;
+    [SerializeField] private AnimationCurve curve;
+    [SerializeField] float shakeDuration;
+
+    private void OnEnable()
     {
-        
+        playerHealth.TookDamage += StartShake;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        playerHealth.TookDamage -= StartShake;
+    }
+
+    private void StartShake(int damage)
+    {
+        StartCoroutine(Shake(damage));
+    }
+
+    private IEnumerator Shake(int damage)
+    {
+        //save original position
+        Vector3 startPosition = transform.position;
+
+        //shake camera for duration
+        float timer = 0;
+        while (timer < shakeDuration)
+        {
+            //get the strength of the shake from the curve and how much damage was taken
+            float strength = curve.Evaluate(timer / shakeDuration) * damage; //IMPORTANT: this assumes that all received damage values are either 1 or 2
+            transform.position = startPosition + Random.insideUnitSphere * strength;
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        //set back to original position
+        transform.position = startPosition;
     }
 }
